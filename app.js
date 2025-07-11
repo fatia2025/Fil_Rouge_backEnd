29c07bdded62d21ccf1db6b3dcb4eb4972ef4e9b
fatia2025 <fatiha.bilot@gmail.com>
2025-07-11 16:30:26 +0200

    merge dev ds branche feat_produit_fatiha

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import connectDB from "./config.js";
 import express from "express";
 import utilisateursRouter from "./Utilisateurs/utilisateursRouter.js";
 import profileRouter from "./Profile/profileRouter.js";
+import produitsRouter from "./Produits/produitsRouter.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/utilisateur", utilisateursRouter);
 app.use("/api/profile", profileRouter);
+app.use("/api/produit", produitsRouter);
 
 connectDB();
 
